784a140b999e728ce55d7f9716a0c346861d7539
Michen <lindenm25@gmail.com>
2020-04-27 14:17:06 +0200

    add: dockable pane for patchjob enumeration and selection

---
+++ IGUIDE/Area.cpp
@@ -0,0 +1,30 @@
+// Area.cpp : implementation file
+//
+
+#include "pch.h"
+#include "IGUIDE.h"
+#include "Area.h"
+
+
+// Area
+
+IMPLEMENT_DYNAMIC(Area, CDockablePane)
+
+Area::Area()
+{
+
+}
+
+Area::~Area()
+{
+}
+
+
+BEGIN_MESSAGE_MAP(Area, CDockablePane)
+END_MESSAGE_MAP()
+
+
+
+// Area message handlers
+
+
---
+++ IGUIDE/Area.h
@@ -0,0 +1,18 @@
+#pragma once
+
+
+// Area
+
+class Area : public CDockablePane
+{
+	DECLARE_DYNAMIC(Area)
+
+public:
+	Area();
+	virtual ~Area();
+
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+
